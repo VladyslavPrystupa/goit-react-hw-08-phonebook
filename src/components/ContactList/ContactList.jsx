63240dc1a5e22816869279ea7a3dba443aba4073
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2023-04-16 17:00:13 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,7 +2,7 @@ import { Loader } from 'components/Loader/Loader';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/operations';
-import { getContacts, getIsLoading } from 'redux/selectors';
+import { getIsLoading } from 'redux/selectors';
 
 import { ContactListItem } from '../ContactListItem/ContactListItem';
 import { List } from './ContactList.styled';
@@ -10,11 +10,10 @@ import { List } from './ContactList.styled';
 export const ContactList = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
-  const contacts = useSelector(getContacts);
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
-  console.log(contacts);
+
   return isLoading ? (
     <Loader />
   ) : (
